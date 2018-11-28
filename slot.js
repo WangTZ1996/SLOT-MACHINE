6df8badcbc231307eb20f19d42668c31e663fe1c
王天柱 <wangtianzhu@wangtianzhudeMacBook-Air.local>
2018-11-28 18:00:29 +0800

    改进版的老虎机

--- slot.js
+++ slot.js
@@ -2,6 +2,7 @@ window.onload = function (){
 	var img1=document.getElementById('1');
 	var img2=document.getElementById('2');
 	var img3=document.getElementById('3');
+	var timer;
 	function changeImg(){
 		var i= Math.ceil(Math.random()*10);
 		img1.src='image/'+i+'.jpg';
@@ -10,14 +11,14 @@ window.onload = function (){
 		var k= Math.floor(Math.random()*10);
 		img3.src='image/'+k+'.jpg'
 	}
+	function run(){
+		timer = setInterval(changeImg,100);
+	}
 	var btn1 = document.getElementById('start');
 
-	btn1.addEventListener('click',function start(){
-		// clearInterval(timer);
-		changeImg();
+	btn1.addEventListener('click',run,false);
+	var btn2 = document.getElementById('stop');
+	btn2.addEventListener('click',function(){
+		clearInterval(timer);
 	},false);
-	// var btn2 = document.getElementById('stop');
-	// btn2.addEventListener('click',function(){
-	// 	clearInterval(timer);
-	// },false);
 }
