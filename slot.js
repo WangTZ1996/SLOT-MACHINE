04b2fb930e85f6bb55618fc9c5de6c72bb715c3d
王天柱 <wangtianzhu@wangtianzhudeMacBook-Air.local>
2018-11-28 16:07:46 +0800

    吃角子老虎机

---
+++ slot.js
@@ -0,0 +1,23 @@
+window.onload = function (){
+	var img1=document.getElementById('1');
+	var img2=document.getElementById('2');
+	var img3=document.getElementById('3');
+	function changeImg(){
+		var i= Math.ceil(Math.random()*10);
+		img1.src='image/'+i+'.jpg';
+		var j= Math.round(Math.random()*10);
+		img2.src='image/'+j+'.jpg';
+		var k= Math.floor(Math.random()*10);
+		img3.src='image/'+k+'.jpg'
+	}
+	var btn1 = document.getElementById('start');
+
+	btn1.addEventListener('click',function start(){
+		// clearInterval(timer);
+		changeImg();
+	},false);
+	// var btn2 = document.getElementById('stop');
+	// btn2.addEventListener('click',function(){
+	// 	clearInterval(timer);
+	// },false);
+}
